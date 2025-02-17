3ac15809da49cccb3b4fae2d37f88fbe6b4e9695
Marsh-bp <128377428+Marsh-bp@users.noreply.github.com>
2025-02-17 22:55:02 +0530

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,17 +1,10 @@
-// blogRoutes.js
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
-const {
-  createBlog,
-  getBlogs,
-  updateBlog,
-  deleteBlog,
-} = require('../controllers/blogController');
+const { createBlog, getBlogs, updateBlog, deleteBlog } = require('../controllers/blogController');
 
-router.post('/', protect, createBlog);
+router.post('/', createBlog);
 router.get('/', getBlogs);
-router.put('/:id', protect, updateBlog);
-router.delete('/:id', protect, deleteBlog);
+router.put('/:id', updateBlog);
+router.delete('/:id', deleteBlog);
 
 module.exports = router;
